ca1e233dee307fd560a4cd0df78d1ecad17f31e4
betty <betty.nabuuso@moringaschool.com>
2021-09-20 18:34:39 +0300

    new commits

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-      $("#submit").submit(function(event) {
+  $("#text-center").submit(function(event) {
         function flavor() {
           var pizzaFlavour = document.getElementById("flavor").value;
           return parseInt(pizzaFlavour);
@@ -14,17 +14,14 @@ $(document).ready(function(){
             return parseInt(pizzaCrust);
           }
 
-          function number() {
-            var pizzaQuantity = document.getElementById("quantity").value;
-            return parseInt(pizzaQuantity);
-          }
-
-          function order(flavor, size, crust, quantity){
-            this.newFlavor = flavor;
-            this.newSize = size;
-            this.newCrust = crust;
-            this.newQuantity = quantity;
-          }
+          //a constructor to create objects/instances of a user's orders
+      function Order(flavor, size, crust, topping, quantity) {
+      this.newFlavor = flavor;
+      this.newSize = size;
+      this.newCrust = crust;
+      this.newTopping = topping;
+      this.newQuantity = quantity;
+    }
       
           var userInput = new Order(flavor(), size(), crust(), quantity());
       
@@ -32,19 +29,19 @@ $(document).ready(function(){
             (userInput.newSize +
               userInput.newCrust +
               userInput.newFlavor)*
-            userInput.newQuantity;
-      
-          //prompts for the user
-          alert("Your charges for Pizza" + totalCost);
-          prompt("enter email");
-          prompt("enter phone number");
-          prompt("enter location");
-          alert("Your pizza will be delivered soon");
-      
-          //a method to reset the form after all operations have been completed
-          $("#text-center").reset();
-      
-          event.preventDefault();
-        });
-      });
+            userInput.newQuantity; 
+
+            //prompts for the user
+    alert("Your charges for Pizza" + totalCost);
+    prompt("enter your email address");
+    prompt("enter your phone number");
+    prompt("enter your location");
+    alert("Your pizza will be delivered");
+
+    //a method to reset the form after all operations have been completed
+    $("#text-center").reset();
+
+    event.preventDefault();
+  });
+});
     
