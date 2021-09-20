4ceeaeb55364af4dbd23bf18fc8da9b3173d8ab2
betty <betty.nabuuso@moringaschool.com>
2021-09-20 05:39:07 +0300

    modified

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+function finalcost(Pepperoni, Salami_Pizza, Fruity_Veggy, PLenty_Cheese)
+    this.Pepperoni=Pepperoni
+    this.Salami_Pizza=Salami_Pizza
+    this.Fruity_Veggyi=Fruity_Veggy
+    this.PLenty_Cheese=PLenty_Cheese
+    
